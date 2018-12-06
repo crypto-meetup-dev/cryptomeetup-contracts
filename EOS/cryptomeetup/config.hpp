2cbcb1ad948dc9ab0eaf796ce03c26e4497e34f0
flyq <flyq951@gmail.com>
2018-12-07 01:32:31 +0800

    add portal

--- EOS/cryptomeetup/config.hpp
+++ EOS/cryptomeetup/config.hpp
@@ -6,6 +6,7 @@ const auto EOS_CONTRACT = "eosio.token"_n;
 const auto EOS_SYMBOL = eosio::symbol("EOS", 4);
 const auto TOKEN_CONTRACT = "dacincubator"_n;
 const auto TOKEN_SYMBOL = eosio::symbol("CMU", 4);
+const auto CMU_SYMBOL = eosio::symbol("CMU", 4);
 
 static constexpr uint128_t MAGNITUDE = 1ll<<32;
 static constexpr time refund_delay = 60;
