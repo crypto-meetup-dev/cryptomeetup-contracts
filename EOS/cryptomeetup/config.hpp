1625e0c3e74cba29fa94d2b76c48cdd4428f099f
flyq <flyq951@gmail.com>
2018-12-13 21:20:14 +0800

    add parent & fix players abi

--- EOS/cryptomeetup/config.hpp
+++ EOS/cryptomeetup/config.hpp
@@ -15,7 +15,6 @@ static constexpr uint128_t LAND_TO_LAST = 60;
 static constexpr uint128_t LAND_TO_POOL = 10;
 static constexpr uint128_t LAND_TO_REF = 3;
 
-static constexpr uint128_t PORTAL_TO_LAST = 60;
 static constexpr uint128_t PORTAL_TO_POOL = 5;
 static constexpr uint128_t PORTAL_TO_PARENT = 2;
 
